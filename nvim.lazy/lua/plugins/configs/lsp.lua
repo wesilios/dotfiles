2a77b56b5bf5ea7f2730d40df291106e7d2ace5f
Que Nguyen <quenguyen10190@gmail.com>
2025-12-13 17:28:19 +0000

    Add nvim-cmp and roslyn configuration for enhanced C# autocompletion

--- nvim.lazy/lua/plugins/configs/lsp.lua
+++ nvim.lazy/lua/plugins/configs/lsp.lua
@@ -1,16 +1,94 @@
 local mason_root = require('mason.settings').current.install_root_dir
 
+-- Setup completion capabilities for LSP
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- Add nvim-cmp capabilities if available
+local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if has_cmp then
+  capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+end
+
+-- LSP Keymaps setup function (called on LspAttach)
+local function setup_lsp_keymaps(bufnr)
+  local map = vim.keymap.set
+  local opts = { buffer = bufnr, silent = true }
+
+  -- Go to definition
+  map('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = 'LSP: Go to definition' }))
+
+  -- Go to declaration
+  map('n', 'gD', vim.lsp.buf.declaration, vim.tbl_extend('force', opts, { desc = 'LSP: Go to declaration' }))
+
+  -- Hover documentation
+  map('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = 'LSP: Hover documentation' }))
+
+  -- Go to implementation
+  map('n', 'gi', vim.lsp.buf.implementation, vim.tbl_extend('force', opts, { desc = 'LSP: Go to implementation' }))
+
+  -- Signature help
+  map('n', '<C-k>', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts, { desc = 'LSP: Signature help' }))
+  map('i', '<C-k>', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts, { desc = 'LSP: Signature help' }))
+
+  -- Rename symbol
+  map('n', '<leader>rn', vim.lsp.buf.rename, vim.tbl_extend('force', opts, { desc = 'LSP: Rename symbol' }))
+
+  -- Code action (includes auto-import)
+  map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, vim.tbl_extend('force', opts, { desc = 'LSP: Code action' }))
+
+  -- Go to references
+  map('n', 'gr', vim.lsp.buf.references, vim.tbl_extend('force', opts, { desc = 'LSP: Go to references' }))
+
+  -- Format buffer
+  map('n', '<leader>lf', function()
+    vim.lsp.buf.format({ async = true })
+  end, vim.tbl_extend('force', opts, { desc = 'LSP: Format buffer' }))
+
+  -- Show diagnostics in floating window
+  map('n', '<leader>e', vim.diagnostic.open_float, vim.tbl_extend('force', opts, { desc = 'LSP: Show diagnostics' }))
+
+  -- Go to previous/next diagnostic
+  map('n', '[d', vim.diagnostic.goto_prev, vim.tbl_extend('force', opts, { desc = 'LSP: Previous diagnostic' }))
+  map('n', ']d', vim.diagnostic.goto_next, vim.tbl_extend('force', opts, { desc = 'LSP: Next diagnostic' }))
+
+  -- Show all diagnostics in location list
+  map('n', '<leader>dl', vim.diagnostic.setloclist, vim.tbl_extend('force', opts, { desc = 'LSP: Diagnostics to loclist' }))
+
+  -- Type definition
+  map('n', '<leader>D', vim.lsp.buf.type_definition, vim.tbl_extend('force', opts, { desc = 'LSP: Type definition' }))
+
+  -- Workspace symbols
+  map('n', '<leader>ws', vim.lsp.buf.workspace_symbol, vim.tbl_extend('force', opts, { desc = 'LSP: Workspace symbols' }))
+end
+
+-- Roslyn LSP configuration for C#
+-- Note: roslyn.nvim plugin handles the actual server setup
+-- This config is for the built-in LSP client settings
 vim.lsp.config('roslyn', {
-  on_attach = function()
-    print('This will run when the server attaches!')
-  end,
+  capabilities = capabilities,
+  -- Roslyn-specific settings for inlay hints and code lens
   settings = {
     ['csharp|inlay_hints'] = {
       csharp_enable_inlay_hints_for_implicit_object_creation = true,
       csharp_enable_inlay_hints_for_implicit_variable_types = true,
+      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+      csharp_enable_inlay_hints_for_types = true,
+      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+      dotnet_enable_inlay_hints_for_literal_parameters = true,
+      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+      dotnet_enable_inlay_hints_for_other_parameters = true,
+      dotnet_enable_inlay_hints_for_parameters = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
     },
     ['csharp|code_lens'] = {
       dotnet_enable_references_code_lens = true,
+      dotnet_enable_tests_code_lens = true,
+    },
+    ['csharp|completion'] = {
+      dotnet_provide_regex_completions = true,
+      dotnet_show_completion_items_from_unimported_namespaces = true,
+      dotnet_show_name_completion_suggestions = true,
     },
   },
 })
@@ -18,6 +96,7 @@ vim.lsp.config('roslyn', {
 -- Command and arguments to start the server.
 vim.lsp.config['luals'] = {
   cmd = { 'lua-language-server' },
+  capabilities = capabilities,
 
   -- Filetypes to automatically attach to.
   filetypes = { 'lua' },
@@ -37,6 +116,16 @@ vim.lsp.config['luals'] = {
       runtime = {
         version = 'LuaJIT',
       },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }
@@ -44,6 +133,7 @@ vim.lsp.config['luals'] = {
 -- Clangd LSP configuration for C/C++
 vim.lsp.config['clangd'] = {
   cmd = { 'clangd' },
+  capabilities = capabilities,
 
   -- Filetypes to automatically attach to.
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
@@ -59,3 +149,25 @@ vim.lsp.config['clangd'] = {
     },
   },
 }
+
+-- LspAttach autocmd to set up keymaps when LSP attaches to buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    -- Set up keymaps for all LSP clients
+    setup_lsp_keymaps(bufnr)
+
+    -- Enable inlay hints if supported
+    if client and client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end
+
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    print(string.format('LSP attached: %s', client and client.name or 'unknown'))
+  end,
+})
